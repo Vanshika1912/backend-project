35d9a94e7c0bab0d8a81b97a076aadad74d7cae5
Vanshika1912 <vanshika191201@gmail.com>
2024-04-05 17:39:34 +0530

    merging the difference

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../utils/constants.js";
 
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect
-        (`${process.env.MONGO_URI}/${DB_NAME}`)
+        (`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`/n MongoDB is connected !! DB HOST: ${connectionInstance.connection.host} `); //use for knowing which host am I connecting
     } catch (error) {
         console.log("MONGODB connection error", error);
-        process.exit(1)
+        process.exit(1);
     }
 }
 
-export default connectDB
+export default connectDB;
